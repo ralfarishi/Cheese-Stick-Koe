34528181a40dd9cc9e198e8c46f2534c74ebf808
Al Farishi <62209112+ralfarishi@users.noreply.github.com>
2023-12-08 23:08:17 +0700

    change: whatsapp message description

--- src/components/WhatsappFloating.js
+++ src/components/WhatsappFloating.js
@@ -4,7 +4,7 @@ export default function WhatsappFloating() {
 	return (
 		<>
 			<div className="floating_btn">
-				<a target="_blank" href="https://wa.link/mvdgi5">
+				<a target="_blank" href="https://wa.link/9gzsvd">
 					<div className="contact_icon">
 						<i className="bi bi-whatsapp my-float"></i>
 					</div>
