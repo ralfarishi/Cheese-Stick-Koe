85e3c1c642c5d95359bedd2c39b1e94d119e468e
Al Farishi <62209112+ralfarishi@users.noreply.github.com>
2023-12-08 23:27:32 +0700

    fix: redundant fragment

--- src/components/WhatsappFloating.js
+++ src/components/WhatsappFloating.js
@@ -2,15 +2,13 @@ import React from "react";
 
 export default function WhatsappFloating() {
 	return (
-		<>
-			<div className="floating_btn">
-				<a target="_blank" href="https://wa.link/9gzsvd">
-					<div className="contact_icon">
-						<i className="bi bi-whatsapp my-float"></i>
-					</div>
-				</a>
-				<p className="text_icon">Pesan sekarang!</p>
-			</div>
-		</>
+		<div className="floating_btn">
+			<a target="_blank" href="https://wa.link/9gzsvd">
+				<div className="contact_icon">
+					<i className="bi bi-whatsapp my-float"></i>
+				</div>
+			</a>
+			<p className="text_icon">Pesan sekarang!</p>
+		</div>
 	);
 }
